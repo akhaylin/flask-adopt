aa8949e03b55a99f8b5866b34f0684d8d4dc08e3
Celeste Kilgore <celestekilgore@gmail.com>
2023-10-06 15:39:15 -0400

    finished add pet form

--- flask-adopt/app.py
+++ flask-adopt/app.py
@@ -2,10 +2,11 @@
 
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import connect_db, Pet, db
+from forms import AddPetForm
 
 app = Flask(__name__)
 
@@ -28,4 +29,31 @@ def list_pets():
     pets = Pet.query.all()
 
     return render_template('pets.html', pets=pets)
+
+@app.route("/add",methods=["GET","POST"])
+def add_pet():
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+        available = form.available.data
+
+        pet = Pet(
+            name=name,
+            species=species,
+            photo_url=photo_url,
+            age=age,
+            notes=notes,
+            available=available)
+
+        db.session.add(pet)
+        db.session.commit()
+        return redirect("/")
+
+    return render_template("add_pet.html",form=form)
 
--- flask-adopt/forms.py
+++ flask-adopt/forms.py
@@ -1,1 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField, BooleanField
+from wtforms.validators import InputRequired
+
 """Forms for adopt app."""
+
+class AddPetForm(FlaskForm):
+    """Form for adding pets."""
+
+    name = StringField("Pet name:",validators=[InputRequired()])
+    species = StringField("Species:",validators=[InputRequired()])
+    photo_url = StringField("Photo URL:")
+    age = SelectField('Age',choices=[
+        ('baby','Baby'),
+        ('young','Young'),
+        ('adult','Adult'),
+        ('senior','Senior')
+        ])
+    notes = TextAreaField("Notes:")
+    available = BooleanField('Is Pet Available?')
